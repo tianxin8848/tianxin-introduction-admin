a082939a699ce89d0ffab9277b8871c72ea807dc
tianxin8848 <1045360566@qq.com>
2026-03-11 10:38:07 +0800

    Fix TypeError: Cannot read properties of null (reading 'homePath')

--- apps/web-ele/src/router/guard.ts
+++ apps/web-ele/src/router/guard.ts
@@ -118,7 +118,7 @@ function setupAccessGuard(router: Router) {
     accessStore.setIsAccessChecked(true);
     const redirectPath = (from.query.redirect ??
       (to.path === preferences.app.defaultHomePath
-        ? userInfo.homePath || preferences.app.defaultHomePath
+        ? userInfo?.homePath || preferences.app.defaultHomePath
         : to.fullPath)) as string;
 
     return {
