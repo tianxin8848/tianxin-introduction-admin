ba524766ca4da6420944e0f7d18cb8f1829b237e
tianxin8848 <1045360566@qq.com>
2026-03-11 12:06:47 +0800

    add:info Page

--- apps/web-ele/src/views/profile/info/index.vue
+++ apps/web-ele/src/views/profile/info/index.vue
@@ -29,32 +29,15 @@
     </div>
     
     <!-- 正常内容 -->
-    <div v-else class="p-6">
-      <h1 class="text-2xl font-bold mb-4">个人信息</h1>
-      
-      <!-- 个人出生年月日卡片 -->
-      <BirthdayCard />
-      
-      <!-- 个人基本信息卡片 -->
-      <BasicInfoCard />
-      
-      <!-- 联系方式卡片 -->
-      <ContactCard />
-      
-      <!-- 向日葵图片卡片 -->
-      <div class="grid grid-cols-1 gap-6">
-        <ImageCard />
-      </div>
+    <div v-if="isAuthenticated" class="p-5">
+      <Info />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
-import BirthdayCard from './infoCom/BirthdayCard.vue';
-import BasicInfoCard from './infoCom/BasicInfoCard.vue';
-import ContactCard from './infoCom/ContactCard.vue';
-import ImageCard from './infoCom/ImageCard.vue';
+import Info from './infoCom/info.vue';
 
 // 状态管理
 const isAuthenticated = ref(false);
--- apps/web-ele/src/views/profile/info/infoCom/info.vue
+++ apps/web-ele/src/views/profile/info/infoCom/info.vue
@@ -53,21 +53,14 @@
 <script setup lang="ts">
 import { ref, defineAsyncComponent } from 'vue'
 
-// 动态导入 Element Plus 组件
-const ElForm = defineAsyncComponent(() => import('element-plus/es/components/form/index'));
-const ElFormItem = defineAsyncComponent(() => import('element-plus/es/components/form-item/index'));
-const ElRow = defineAsyncComponent(() => import('element-plus/es/components/row/index'));
-const ElCol = defineAsyncComponent(() => import('element-plus/es/components/col/index'));
-const ElTable = defineAsyncComponent(() => import('element-plus/es/components/table/index'));
-const ElTableColumn = defineAsyncComponent(() => import('element-plus/es/components/table-column/index'));
+// 导入 Element Plus 组件
+import { ElForm, ElFormItem, ElRow, ElCol, ElTable, ElTableColumn } from 'element-plus';
 
 // 导入样式
 import 'element-plus/es/components/form/style/css';
-import 'element-plus/es/components/form-item/style/css';
 import 'element-plus/es/components/row/style/css';
 import 'element-plus/es/components/col/style/css';
 import 'element-plus/es/components/table/style/css';
-import 'element-plus/es/components/table-column/style/css';
 
 // 表单模型
 const form = ref({})
