e12e07e0e1b6ed376d93fec4d27d8fc42d0ad5e8
tianxin8848 <1045360566@qq.com>
2026-03-11 11:59:22 +0800

    Fix Element Plus component import paths

--- apps/web-ele/src/views/profile/info/infoCom/info.vue
+++ apps/web-ele/src/views/profile/info/infoCom/info.vue
@@ -54,12 +54,12 @@
 import { ref, defineAsyncComponent } from 'vue'
 
 // 动态导入 Element Plus 组件
-const ElForm = defineAsyncComponent(() => import('element-plus/es/components/form/index.vue'));
-const ElFormItem = defineAsyncComponent(() => import('element-plus/es/components/form-item/index.vue'));
-const ElRow = defineAsyncComponent(() => import('element-plus/es/components/row/index.vue'));
-const ElCol = defineAsyncComponent(() => import('element-plus/es/components/col/index.vue'));
-const ElTable = defineAsyncComponent(() => import('element-plus/es/components/table/index.vue'));
-const ElTableColumn = defineAsyncComponent(() => import('element-plus/es/components/table-column/index.vue'));
+const ElForm = defineAsyncComponent(() => import('element-plus/es/components/form/index'));
+const ElFormItem = defineAsyncComponent(() => import('element-plus/es/components/form-item/index'));
+const ElRow = defineAsyncComponent(() => import('element-plus/es/components/row/index'));
+const ElCol = defineAsyncComponent(() => import('element-plus/es/components/col/index'));
+const ElTable = defineAsyncComponent(() => import('element-plus/es/components/table/index'));
+const ElTableColumn = defineAsyncComponent(() => import('element-plus/es/components/table-column/index'));
 
 // 导入样式
 import 'element-plus/es/components/form/style/css';
