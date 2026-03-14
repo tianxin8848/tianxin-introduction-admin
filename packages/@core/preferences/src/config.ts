42c1d7c133aa0869d0d1e75c3f301e2e630942fa
tianxin8848 <1045360566@qq.com>
2026-03-14 23:09:42 +0800

    fix:topic name

--- packages/@core/preferences/src/config.ts
+++ packages/@core/preferences/src/config.ts
@@ -28,7 +28,7 @@ const defaultPreferences: Preferences = {
     layout: 'sidebar-nav',
     locale: 'zh-CN',
     loginExpiredMode: 'page',
-    name: 'Vben Admin',
+    name: 'TaSei Page',
     preferencesButtonPosition: 'auto',
     watermark: false,
     watermarkContent: '',
